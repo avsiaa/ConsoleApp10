c239b5fe97612be7b9d0abb472d1a6835c5645e4
ketamine <ketamine@IDEAPAD-GAMING>
2025-03-26 12:12:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Method1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+
+    partial class Methods
+    {
+
+        public static void firstBlock()
+        {
+            int[] array = GetUserInputForFirstBlock();
+
+            if (array.Length < 2) return;
+
+            int indexOfFirstMin = Array.IndexOf(array, array.Min());
+            int indexOfLastMax = Array.LastIndexOf(array, array.Max());
+
+            int newLength = (indexOfFirstMin + 1) + (array.Length - indexOfLastMax);
+            int[] resultArray = new int[newLength];
+            int j = 0;
+
+            for(int i = 0; i <= indexOfFirstMin; i++)
+            {
+                resultArray[j++] = array[i];
+            }
+
+            for(int i = indexOfLastMax; i < array.Length; i++)
+            {
+                resultArray[j++] = array[i];
+            }
+
+            Console.WriteLine(string.Join(", ", resultArray));
+        }
+        
+
+        private static int[] GetUserInputForFirstBlock()
+        {
+            Console.WriteLine("Введіть элементи масиву через пробіл: ");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            return array;
+        }
+
+
+
+        public static void secondBlock() {
+            int[][] array = GetUserInputForSecondBlock();
+
+            int count = 0;
+            foreach (var row in array)
+            {
+                if (row.Length > 0)
+                {
+                    count++;
+                }
+            }
+
+            
+            int[][] result = new int[count][];
+            int index = 0;
+
+         
+            foreach (var row in array)
+            {
+                if (row.Length > 0)
+                {
+                    result[index++] = row;
+                }
+            }
+
+            ShowJaggedArray(result);
+
+            
+
+        }
+
+        private static int[][] GetUserInputForSecondBlock()
+        {
+            Console.WriteLine("Введіть розмір зубчастого масиву: ");
+            int size = int.Parse( Console.ReadLine());
+            int[][] array = new int[size][];
+            Console.WriteLine("Починайте вводити зубчастий масив: ");
+            for (int i = 0; i < size; i++)
+            {
+                string row = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(row))
+                {
+                    array[i] = new int[0];
+                }
+                else
+                {
+                    array[i] = row.Trim().Split(' ').Select(int.Parse).ToArray();
+                }
+            }
+
+            return array;
+        
+        }
+
+        private static void ShowJaggedArray(int[][] array)
+        {
+            Console.WriteLine("Ваш зубчастий массив: ");
+            foreach (var row in array)
+            {
+                Console.WriteLine(string.Join("\t", row));
+
+            }
+        }
+    }
+     
+    }
+
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+                Console.WriteLine("Введіть число, який блок ви хочете побачити? (1,2): ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Блок 1");
+                        Console.WriteLine("Завдання якого студента ви хочете побачити?: \n" +
+                            "1. Вольвач Анастасія - варіант 7 \n" +
+                            "2. Mmmm Mmmm - варіант N \n" +
+                            "3. Gggg Gggg - варіант N \n");
+                        int choiceOfTask = int.Parse(Console.ReadLine());
+
+                        switch (choiceOfTask)
+                        {
+                            case 1:
+                                Console.WriteLine("Виконую завдання під варіантом 7: ");
+                                Methods.firstBlock();
+                                break;
+
+                            default: return;
+                                
+                        }
+                        
+                    break;
+
+                    case 2:
+                    Console.WriteLine("Блок 2");
+                        Console.WriteLine("Завдання якого студента ви хочете побачити?: \n" +
+                            "1. Вольвач Анастасія - варіант 10 \n" +
+                            "2. Mmmm Mmmm - варіант N \n" +
+                            "3. Gggg Gggg - варіант N \n");
+                        int choiceOfTask2 = int.Parse(Console.ReadLine());
+                        switch (choiceOfTask2)
+                        {
+                            case 1:
+                                Console.WriteLine("Виконую варіант 10: ");
+                                Methods.secondBlock();
+                                break;
+                                
+                               
+                        }
+                        break;
+                    default: 
+                        isTrue = false;
+                        break;
+
+                }
+            }
+
+
+        }
+    }
+}
