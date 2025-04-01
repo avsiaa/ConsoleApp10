9fcf8d98610883fe5b2eb37e388c245d22d30645
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-04-01 10:01:04 +0300

    видалила мейн та підлаштувала свою частину коду під спільну частину

--- ConsoleApp10/Method2.cs
+++ ConsoleApp10/Method2.cs
@@ -8,50 +8,19 @@ namespace ConsoleApp10
 {
     partial class Methods
     {
-        static void Main()
+        public static void Olga1(bool isRandom)
         {
-            int n = GetArraySize();
-            int[] arr = ChooseArrVedenia(n);
-
-            (int min, int max) = MinMax(arr);
-            int[] newArr = NewArray(arr, min, max);
-
+            int[] array = isRandom ? ArrayRandom.GetArrayRandom() : ArrayHelper.GetArray();
+            if(array.Length ==0)
+            {
+                Console.WriteLine("Масив порожній! Введіть дані спочатку.");
+                return;
+            }
+            (int min, int max) = MinMax(array);
+            int[] newArr = NewArray(array, min, max);
             PrintArray(newArr);
         }
-        static int GetArraySize()
-        {
-            Console.Write("Введіть розмір масиву: ");
-            return int.Parse(Console.ReadLine());
-        }
-        static int[] ChooseArrVedenia(int size)
-        {
-            Console.Write("Оберіть спосіб заповнення масиву (1 - вручну, 2 - рандомно): ");
-            int choice = int.Parse(Console.ReadLine());
-
-            return choice == 1 ? ManualArray(size) : RandomArray(size);
-        }
-
-        static int[] ManualArray(int size)
-        {
-            int[] arr = new int[size];
-            Console.WriteLine("Введіть елементи масиву:");
-            for (int i = 0; i < size; i++)
-            {
-                arr[i] = int.Parse(Console.ReadLine());
-            }
-            return arr;
-        }
-        static int[] RandomArray(int size)
-        {
-            int[] arr = new int[size];
-            Random rand = new Random();
-            for (int i = 0; i < size; i++)
-            {
-                arr[i] = rand.Next(-1000000, 1000000);
-            }
-            Console.WriteLine("Згенерований масив: " + string.Join(" ", arr));
-            return arr;
-        }
+        
         static (int min, int max) MinMax(int[] arr)
         {
             int min = arr[0], max = arr[0];
