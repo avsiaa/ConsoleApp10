47b06ef710f955083614b7c6d6b8d55d8069beee
Volvach Anastasia <avsiaa@icloud.com>
2025-04-01 20:16:31 +0300

    Допрацювала метод для випадкового заповнювання масиву (додала можливість вказувати мінімальне та максимальне значення при заповненні)

--- ConsoleApp10/Random.cs
+++ ConsoleApp10/Random.cs
@@ -7,16 +7,21 @@ namespace ConsoleApp10
         private static int[] arrayRandom;
         public static void InputArrayRandom()
         {
-            Console.Write("Введіть, скільки елементів згенерувати випадково: ");
+            Console.Write("Введіть, скільки елементів згенерувати випадково: ");//Допрацювала метод для випадкового заповнювання масиву (додала можливість вказувати мінімальне та максимальне значення при заповненні)
             int size = int.Parse(Console.ReadLine());
 
+            Console.Write("Введіть мінімальне значення: ");
+            int minValue = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть максимальне значення: ");
+            int maxValue = int.Parse(Console.ReadLine());
             int[] arr = new int[size];
             Random rand = new Random();
             for (int i = 0; i < size; i++)
             {
-                arr[i] = rand.Next(-1000000, 1000000);// додати тут
+                arr[i] = rand.Next(minValue, maxValue + 1);
             }
-            Console.WriteLine("Масив успішно збережений!");
+            Console.WriteLine("Масив успішно збережений");
             arrayRandom = arr;
 
             Console.Write("Згенерований масив: ");
@@ -25,6 +30,7 @@ namespace ConsoleApp10
                 Console.Write(num + " ");
             }
         }
+
         public static int[] GetArrayRandom()
         {
             return arrayRandom ?? new int[0];
