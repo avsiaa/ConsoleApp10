06654dd3c3f8610adc12c4f5cabfcba2fad18a77
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-04-01 10:21:00 +0300

    Настя, зроби так, щоб користувач сам вносив верхню та нижню межу значень для рандому

--- ConsoleApp10/Random.cs
+++ ConsoleApp10/Random.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp10
             Random rand = new Random();
             for (int i = 0; i < size; i++)
             {
-                arr[i] = rand.Next(-1000000, 1000000);
+                arr[i] = rand.Next(-1000000, 1000000);// додати тут
             }
             Console.WriteLine("Масив успішно збережений!");
             arrayRandom = arr;
