b011dc37837d4df24f6930419f235658b78de0ff
Liza Bondarenko <bondarenkoliza705@gmail.com>
2025-04-01 18:22:46 +0300

    розділила код на окремі методи в своєму блоці

--- ConsoleApp10/Method3.cs
+++ ConsoleApp10/Method3.cs
@@ -6,24 +6,25 @@ namespace ConsoleApp10
     {
         public static void firstBlockBond(bool isRandom)
         {
-            int[] array = isRandom ? ArrayRandom.GetArrayRandom() : ArrayHelper.GetArray();// тут
-            if (array.Length == 0)
-            {
-                Console.WriteLine("Масив порожній! Введіть дані спочатку.");
-                return;
-            }
-
+            int[] array = isRandom ? ArrayRandom.GetArrayRandom() : ArrayHelper.GetArray();
             int newSize = array.Length / 2 + (array.Length % 2);
             int[] resultArray = new int[newSize];
-            int index = 0;
 
+            NewInputArrayRandom(ref resultArray, array);
+            Print(resultArray);
+        }
+        static void Print(int[] resultArray)
+        {
+            Console.WriteLine("Масив без елементів із парними індексами:");
+            Console.WriteLine(string.Join(" ", resultArray));
+        }
+        static void NewInputArrayRandom(ref int[] resultArray, int[] array)
+        {
+            int index = 0;
             for (int i = 1; i < array.Length; i += 2)
             {
                 resultArray[index++] = array[i];
             }
-
-            Console.WriteLine("Масив без елементів із парними індексами:");// і тут
-            Console.WriteLine(string.Join(" ", resultArray));
         }
     }
 }
