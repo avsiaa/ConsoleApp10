3209fbad1cf2cdf8c24750636c57affeb6ec076a
Liza Bondarenko <bondarenkoliza705@gmail.com>
2025-04-02 10:48:40 +0300

    прибрала 0

--- ConsoleApp10/Method3.cs
+++ ConsoleApp10/Method3.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp10
         public static void firstBlockBond(bool isRandom)
         {
             int[] array = isRandom ? ArrayRandom.GetArrayRandom() : ArrayHelper.GetArray();
-            int newSize = array.Length / 2 + (array.Length % 2);
+            int newSize = array.Length / 2 ;
             int[] resultArray = new int[newSize];
 
             NewInputArrayRandom(ref resultArray, array);
